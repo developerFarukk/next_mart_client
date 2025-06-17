c4b62834a4c611dce771b3708e5c7dd32cd222c8
FARUK2059 <omarfaruk34552@gmail.com>
2025-06-18 01:17:43 +0600

    shop formsetup

---
+++ src/app/(WithCommonLayout)/create-shop/page.tsx
@@ -0,0 +1,17 @@
+import CreateShopForm from "@/components/modules/shop/create-shop/CreateShopForm";
+
+
+
+
+const CreateShopPage = () => {
+
+    return (
+        <div>
+            <div className="flex justify-center items-center">
+                <CreateShopForm />
+            </div>
+        </div>
+    );
+};
+
+export default CreateShopPage;
